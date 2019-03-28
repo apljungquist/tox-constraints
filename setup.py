b7c3ca76593922d1d6add65c93c1d22b51efb5c5
AP Ljungquist <ap@ljungquist.eu>
2019-03-28 23:11:35 +0100

    Documents dependencies
    Stable, concrete dependencies are listed separately from abstract dependencies to
    facilitate updates.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
+with open("requirements/install_requires.txt") as fp:
+    install_requires = list(fp)
+
 setuptools.setup(
     name="tox-constraints",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    install_requires=["tox"],
+    install_requires=install_requires,
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     entry_points={"tox": ["constraints = tox_constraints"]},
