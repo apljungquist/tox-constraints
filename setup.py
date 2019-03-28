70056d7445c1216d7aa60c241242ce69f28f73d2
AP Ljungquist <ap@ljungquist.eu>
2019-03-28 22:29:15 +0100

    Adds initial implementation

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setuptools.setup(
     name="tox-constraints",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
+    install_requires=["tox"],
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
+    entry_points={"tox": ["constraints = tox_constraints"]},
 )
