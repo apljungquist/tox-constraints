fa95f4f32e321e3a0d2ca50754906df19c212a21
AP Ljungquist <ap@ljungquist.eu>
2019-04-08 20:27:49 +0200

    Removes setuptools_scm
    ... to make it evident what files are explicitly included by the
        manifest.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
+
 with open("requirements/install_requires.txt") as fp:
     install_requires = list(fp)
 
 setuptools.setup(
     name="tox-constraints",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
+    version="0.5.2.dev",
     install_requires=install_requires,
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
