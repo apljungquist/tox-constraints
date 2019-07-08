a4152335563bc60c09d384db9720cc58aaf3bce4
AP Ljungquist <ap@ljungquist.eu>
2019-07-08 08:19:24 +0200

    Adds test tracking if sample projects work or not
    ... to make it easier to know what changes to develop and harder to accidentally break
        something.

---
+++ tests/test_sample_projects.py
@@ -0,0 +1,32 @@
+"""Tests checking that this project interacts as expected with other projects.
+
+A sort ot system test if you will.
+"""
+import pathlib
+import subprocess
+
+import pytest  # type: ignore
+
+_PROJECT_ROOT = pathlib.Path(__file__).parents[1]
+_SAMPLE_PROJECTS = _PROJECT_ROOT / "sample_projects"
+_PROJECT_NAMES = [
+    # Keep slow tests and expected failures in the back to fail fast
+    pytest.param("less", marks=[]),
+    pytest.param("xfmt", marks=[]),
+    pytest.param("more", marks=[pytest.mark.xfail(strict=True)]),
+    pytest.param("pypackage", marks=[pytest.mark.xfail(strict=True)]),
+    pytest.param("sprig", marks=[pytest.mark.xfail(strict=True)]),
+]
+
+
+@pytest.mark.parametrize("project_name", _PROJECT_NAMES)
+def test_does_not_break(project_name):
+    """Smoke test
+
+    Ensures that having tox-constraints installed does not interfere in projects that
+    do not use it.
+
+    Ensures that for projects that use tox constraints do not smoke.
+    This is especially useful in combination with offensive programming mechanisms.
+    """
+    subprocess.check_call(["tox", "-r"], cwd=_SAMPLE_PROJECTS / project_name)
